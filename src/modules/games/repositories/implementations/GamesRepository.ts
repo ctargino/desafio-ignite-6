0d9abd2908f0c481389c84fd0ff1f703417d7cc2
ctargino <c.targino@gmail.com>
2021-06-01 22:20:01 -0300

    fix: games respository queries

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -6,25 +6,29 @@ import { Game } from '../../entities/Game';
 import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
-  private repository: Repository<Game>;
+  private repository: Repository<Game>
+  private userRepository: Repository<User>
 
   constructor() {
     this.repository = getRepository(Game);
+    this.userRepository = getRepository(User)
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games')
+      .where('LOWER(title) ILIKE LOWER(:title)', {title: `%${param}%`})
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) from games`); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return this.userRepository
+      .createQueryBuilder('users')
+      .leftJoinAndSelect('users.games', 'games')
+      .where('games.id = :id', {id}).getMany()
   }
 }
